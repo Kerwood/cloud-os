abd3010c980b55194e89b98a74fbbcc29dade1ce
Patrick Kerwood <patrick@kerwood.dk>
2024-03-26 22:45:16 +0100

    removed outcommented lines

--- config/scripts/setup-google-chrome-repo.sh
+++ config/scripts/setup-google-chrome-repo.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-# curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-# rpm-ostree install -y google-chrome-stable_current_x86_64.rpm
-
 rpm --import https://dl.google.com/linux/linux_signing_key.pub
 
 cat << EOF | tee /etc/yum.repos.d/google-chrome.repo
