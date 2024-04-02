aacb5dbaa6b09b3d8d1489a51d73489b18e00d7e
Patrick Kerwood <patrick@kerwood.dk>
2024-04-02 16:59:09 +0200

    fixed kind url

--- config/scripts/install-kind.sh
+++ config/scripts/install-kind.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-curl -sSL -o kind https://github.com/kubernetes-sigs/kind/latest/download/kind-linux-amd64
+curl -sSL -o kind https://github.com/kubernetes-sigs/kind/releases/latest/download/kind-linux-amd64
 chmod +x kind
 mv -v kind /usr/bin/
