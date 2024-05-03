2ecf3312d732bce4a416b9eb5bde8f6c659bcfe8
Patrick Kerwood <patrick@kerwood.dk>
2024-05-03 14:14:22 +0200

    fixed gum issue

--- config/scripts/install-gum.sh
+++ config/scripts/install-gum.sh
@@ -7,4 +7,4 @@ FILENAME=gum_${VERSION}_Linux_x86_64
 
 curl -OL "https://github.com/charmbracelet/gum/releases/download/v${VERSION}/${FILENAME}.tar.gz"
 tar -zxvf "$FILENAME.tar.gz"
-mv -v gum /usr/bin
+mv -v "$FILENAME/gum" /usr/bin
