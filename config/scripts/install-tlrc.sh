7efb8b6ab0ddab67f0ec1f85275bdea4ddfbe080
Patrick Kerwood <patrick@kerwood.dk>
2024-05-12 22:24:38 +0200

    tldr fix

--- config/scripts/install-tlrc.sh
+++ config/scripts/install-tlrc.sh
@@ -7,4 +7,4 @@ FILENAME=tlrc-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz
 
 curl -OL "https://github.com/tldr-pages/tlrc/releases/download/v${VERSION}/${FILENAME}"
 tar -zxvf "$FILENAME"
-mv -v "tlrc-v${VERSION}-x86_64-unknown-linux-gnu/tldr" /usr/bin
+mv -v tldr /usr/bin
