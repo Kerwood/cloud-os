08b234548fe8193bfbc8017558418344d584a682
Patrick Kerwood <patrick@kerwood.dk>
2024-06-15 12:54:34 +0200

    fixed atuin install issue

--- config/scripts/install-atuin.sh
+++ config/scripts/install-atuin.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-VERSION=$(curl -sL "https://api.github.com/repos/atuinsh/atuin/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-FILENAME=atuin-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz
-
-curl -OL "https://github.com/atuinsh/atuin/releases/download/v${VERSION}/${FILENAME}"
-tar -zxvf "$FILENAME"
-mv -v "atuin-v${VERSION}-x86_64-unknown-linux-gnu/atuin" /usr/bin
+FOLDERNAME="atuin-x86_64-unknown-linux-gnu"
+curl -LO https://github.com/atuinsh/atuin/releases/latest/download/$FOLDERNAME.tar.gz
+tar -zxvf $FOLDERNAME.tar.gz
+echo mv -v $FOLDERNAME/atuin /usr/bin
